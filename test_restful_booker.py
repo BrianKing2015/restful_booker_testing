34c960d9799216d3074bda295545b7b951d292d4
Brian King <brian.king@broadsign.com>
2022-01-01 10:25:10 -0500

    Adding test for creating booking with faker info

--- test_restful_booker.py
+++ test_restful_booker.py
@@ -1,8 +1,10 @@
+import faker
 import requests
 import json
 from urllib.parse import urljoin
 import random
-from faker.providers.person.en import Provider
+from faker.providers.person.en import Provider as person_faker
+
 
 URL = "https://restful-booker.herokuapp.com"
 
@@ -35,10 +37,46 @@ def test_bookings_return():
     assert json.loads(response.text)[0]['bookingid']
 
 
+def test_creating_booking():
+    url = urljoin(URL, "booking")
+    fake = faker.Faker()
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    price = random.randint(0, 99999)
+    deposit = random.choice([True, False])
+    check_in = str(fake.date_time_this_decade())
+    check_out = str(fake.date_time_this_decade())
+    needs = fake.word()
+    payload = json.dumps({
+        "firstname": first_name,
+        "lastname": last_name,
+        "totalprice": price,
+        "depositpaid": deposit,
+        "bookingdates": {
+            "checkin": check_in,
+            "checkout": check_out
+        },
+        "additionalneeds": needs
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    json_response = json.loads(response.text)
+    assert response.status_code == 200
+    assert json_response['booking']['firstname'] == first_name
+    assert json_response['booking']['lastname'] == last_name
+    assert json_response['booking']['totalprice'] == price
+    assert json_response['booking']['depositpaid'] == deposit
+    assert json_response['booking']['bookingdates']['checkin'] == check_in.split(' ')[0]
+    assert json_response['booking']['bookingdates']['checkout'] == check_out.split(' ')[0]
+    assert json_response['booking']['additionalneeds'] == needs
+
 
 
 if __name__ == '__main__':
     # token = create_auth_token(password='password123')
     # https://restful-booker.herokuapp.com/apidoc/index.html
     # random_name = Provider.first_names[random.randint(0, len(Provider.first_names))]
-    test_bookings_return()
+    # test_bookings_return()
+    thing = test_creating_booking()
