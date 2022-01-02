992af5478743e39ad5e9f9959ee07ad600b453fa
Brian King <brian.king@broadsign.com>
2022-01-02 11:40:04 -0500

    Added test for updating booking

--- test_restful_booker.py
+++ test_restful_booker.py
@@ -5,7 +5,6 @@ import requests
 import json
 from urllib.parse import urljoin
 import random
-from faker.providers.person.en import Provider as person_faker
 
 
 URL = "https://restful-booker.herokuapp.com"
@@ -46,22 +45,43 @@ def test_search_by_name():
     create_booking_url = urljoin(URL, "booking")
     check_in, check_out, deposit, first_name, headers, last_name, needs, payload, price = create_booking_json()
     create_call = requests.request("POST", create_booking_url, headers=headers, data=payload)
-
     json_create_call = json.loads(create_call.text)
+
     search_url = urljoin(URL, f"booking?firstname={first_name}&lastname={last_name}")
     search_call = requests.request("GET", search_url)
-
     json_search_booking_call = json.loads(search_call.text)
+
     for entry in json_search_booking_call:
         booking_id_list.append(entry['bookingid'])
     assert json_create_call['bookingid'] in booking_id_list
+
+
+def test_update_booking():
+    create_booking_url = urljoin(URL, "booking")
+    check_in, check_out, deposit, first_name, headers, last_name, needs, payload, price = create_booking_json()
+    create_call = requests.request("POST", create_booking_url, headers=headers, data=payload)
+    json_create_call = json.loads(create_call.text)
+
+    auth_token = create_auth_token('password123')
+    update_booking_url = urljoin(URL, f"booking/{json_create_call['bookingid']}")
+    c_in, c_out, dep, f_name, haders, l_name, nds, pload, prce = update_booking_json()
+    update_call = requests.request("PUT", update_booking_url, headers=haders, data=pload)
+    json_response = json.loads(update_call.text)
+    assert update_call.status_code == 200
+    assert json_response['firstname'] == f_name
+    assert json_response['lastname'] == l_name
+    assert json_response['totalprice'] == prce
+    assert json_response['depositpaid'] == dep
+    assert json_response['bookingdates']['checkin'] == str(c_in).split(' ')[0]
+    assert json_response['bookingdates']['checkout'] == str(c_out).split(' ')[0]
+    assert json_response['additionalneeds'] == nds
 
 
 def create_booking_json():
     fake = faker.Faker()
     first_name = fake.first_name()
     last_name = fake.last_name()
-    price = random.randint(0, 99999)/100
+    price = random.randint(0, 99999)
     deposit = random.choice([True, False])
     check_in = fake.date_time_this_decade()
     check_out = check_in + timedelta(days=random.randint(0, 240))
@@ -78,6 +98,34 @@ def create_booking_json():
         "additionalneeds": needs
     })
     headers = {
+        'Content-Type': 'application/json'
+    }
+    return check_in, check_out, deposit, first_name, headers, last_name, needs, payload, price
+
+
+def update_booking_json():
+    fake = faker.Faker()
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    price = random.randint(0, 99999)
+    deposit = random.choice([True, False])
+    check_in = fake.date_time_this_decade()
+    check_out = check_in + timedelta(days=random.randint(0, 240))
+    needs = fake.word()
+    payload = json.dumps({
+        "firstname": first_name,
+        "lastname": last_name,
+        "totalprice": price,
+        "depositpaid": deposit,
+        "bookingdates": {
+            "checkin": str(check_in),
+            "checkout": str(check_out)
+        },
+        "additionalneeds": needs
+    })
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': 'Basic YWRtaW46cGFzc3dvcmQxMjM=',
         'Content-Type': 'application/json'
     }
     return check_in, check_out, deposit, first_name, headers, last_name, needs, payload, price
@@ -101,4 +149,4 @@ if __name__ == '__main__':
     # https://restful-booker.herokuapp.com/apidoc/index.html
     # random_name = Provider.first_names[random.randint(0, len(Provider.first_names))]
     # test_bookings_return()
-    thing = test_search_by_name()
+    thing = test_update_booking()
